d0ec7c9b9ce41c3ad95c2494a8c9b62f157df59f
Bemdemsik <lebedev151a@mail.ru>
2024-02-24 15:52:40 +0300

    -add client -add server -add bd

---
+++ server/src/main.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+const app = express()
+
+app.use(express.json())
+
+const PORT = process.env.PORT
+
+app.listen(PORT, () => {
+    console.log('Server starting on ' + PORT)
+})
